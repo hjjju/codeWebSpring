09d1e38f0186de50cea221666f5a5f17b8a7fee7
heo <stringjuh@gmail.com>
2021-05-06 17:35:56 +0900

    jdbc 설정

--- jex00/src/main/java/org/zerock/sample/Chef.java
+++ jex00/src/main/java/org/zerock/sample/Chef.java
@@ -4,7 +4,14 @@ import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
-@Component
+/*
+ * @Component : 해당클래스가 스프링에서 객체로만들어서관리하는대상임음명시
+ * @Component가 있는 클래스를 스프링이 읽어 주도록 @ComponentScan을 통해서 지정되어있으므로 해당 패키지에있는 클래스들을 조사하면서
+ *  @Component가 존재하는 클래스들을 객체로 생성해서 빈으로 관리.
+ * */
+
+
+@Component    
 @Data
 public class Chef {
 	//restaurant클래스는 chef를 주입받아야함
--- jex00/src/main/java/org/zerock/sample/SampleTest.java
+++ jex00/src/main/java/org/zerock/sample/SampleTest.java
@@ -13,13 +13,21 @@ import org.zerock.config.RootConfig;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
-
+//@Runwith 테스트시 필요한 클래스 지정
 @RunWith(SpringJUnit4ClassRunner.class)		// 현재 테스트코드가 스프링을 실행하는 역할을 할것이 이라는 것을  @RunWith  어노테이션으로 표시
+/*
+ * @ContextConfiguration : 테스트관련 어노테이션 
+ * 스프링이 실행되면서 어떤설정 정보를 읽어 들여야하는지 명시
+ * 속성: locations를 이용 문자열의배열로 xml설정파일 명시 또는
+ * class 속성: @Configuration 이 적용된 클래스를 지정해 줄수도 있다.
+ * 
+ * */
 @ContextConfiguration(classes= {RootConfig.class})
 @Log4j
 public class SampleTest {
 	
 	@Setter(onMethod_ = { @Autowired } )	//@Autowired  해당 인스턴스 변수가 스프링으로 부터 자동으로 주입해 달라는 표시
+	//onMethod setter메섣의 생성 시 메서드에 추가할 어노테이션 지정
 	private Restaurant restaurant;
 	
 	@Test			// junit에서 테스트 대상을 표시하는 어노테이션
