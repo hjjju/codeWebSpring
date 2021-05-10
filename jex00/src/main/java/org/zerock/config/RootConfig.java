56cea856c2d34e364bc5ff5703a4aaccb9b129aa
hjjju <strinjuh@gmail.com>
2021-05-10 17:59:45 +0900

    MyBatis설정

--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -2,6 +2,9 @@ package org.zerock.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +14,7 @@ import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"org.zerock.sample"})
+@MapperScan(basePackages = {"org.zerock.mapper"})
 public class RootConfig  {
 	
 	@Bean
@@ -27,4 +31,11 @@ public class RootConfig  {
 		
 	}
 	
+	@Bean
+	public SqlSessionFactory sqlSessionFActory() throws Exception{
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		
+		return (SqlSessionFactory) sqlSessionFactory.getObject();
+	}
 }
--- jex00/src/main/java/test/java/DataSourceTests.java
+++ jex00/src/main/java/test/java/DataSourceTests.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,14 +26,30 @@ import lombok.extern.log4j.Log4j;
 public class DataSourceTests {
 	@Setter(onMethod_ = {@Autowired})
 	private DataSource dataSource;
+	
+	@Setter(onMethod_= {@Autowired})
+	private SqlSessionFactory sqlSessionFactory;
+
+	
+	  @Test public void testConnection() { try(Connection con =
+	  dataSource.getConnection()) { log.info(con); } catch (Exception e) {
+	  fail(e.getMessage()); }
+	  
+	  }
+	 
 	@Test
-	public void testConnection() {
-		try(Connection con = dataSource.getConnection()) {
-			log.info(con);
+	public void testMyBatis() {
+		try(SqlSession session = sqlSessionFactory.openSession();
+				
+				Connection con = session.getConnection();
+				
+				) {
+			log.info("session: " + session);
+			log.info("con: "+con);
+			
 		} catch (Exception e) {
 			fail(e.getMessage());
-		} 
-		
+		}
 	}
 	
 	
